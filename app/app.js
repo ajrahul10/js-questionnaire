b37419f02a9e89d8a95c7b6f44d86810fcf0f1a6
Rahul Ajmani <rahulajmani910@gmail.com>
2022-02-02 13:22:06 +0530

    addding category svgs, updating results and css

--- app/app.js
+++ app/app.js
@@ -1,8 +1,13 @@
 // number of questions
-let totalQuestions = 80;
-let paginationNumOfViews = 4
+let totalQuestions = 80
+let totalTablePages = 4
+let currPageNum = 1
 let questions = []
 let results = {}
+const CATEGORY_ACTIVIST = 'activist'
+const CATEGORY_THEORIST = 'theorist'
+const CATEGORY_REFLECTOR = 'reflector'
+const CATEGORY_PRAGMATIST = 'pragmatist'
 
 // read the questions csv data
 $(document).ready(function() {
@@ -45,7 +50,7 @@ const renderData = () => {
         $('#tbody').append(`
             <tr>
                 <td class="question-number-label">${idx + 1}</td>
-                <td data-label="QUESTION #1" colspan="3">${question.question}</td>
+                <td data-label="QUESTION #${idx + 1}" colspan="3">${question.question}</td>
                 <td data-label="AGREE">
                     <label class='container'>
                         <input type="radio" name="ques${idx}" value="1" class="option-green">
@@ -63,21 +68,63 @@ const renderData = () => {
     })
 }
 
+const paginateNextQuestions = () => {
+    const currPage = $('#nav a.active')[0]?.innerHTML
+    var rowsShown = totalQuestions / totalTablePages;
+    var startItem = currPage * rowsShown;
+    var endItem = startItem + rowsShown;
+    $('#questionnaire-table tbody tr').css('opacity','0.0').hide().slice(startItem, endItem).
+    css('display','table-row').animate({opacity:1}, 300);
+    const nextPage = currPage
+    if(nextPage <= totalTablePages) {
+        $('#nav a.active').removeClass('active')
+        $($('#nav a')[nextPage]).addClass('active');
+    }
+    if((Number(nextPage) + 1) === totalTablePages) {
+        $('#nextPage').attr("disabled", true);
+    } else {
+        $('#nextPage').removeAttr("disabled");
+    }
+    $('#prevPage').removeAttr("disabled");
+}
+
+const paginatePrevQuestions = () => {
+    const currPage = $('#nav a.active')[0]?.innerHTML
+    const prevPage = currPage - 2
+    var rowsShown = totalQuestions / totalTablePages;
+    var startItem = prevPage * rowsShown;
+    var endItem = startItem + rowsShown;
+    $('#questionnaire-table tbody tr').css('opacity','0.0').hide().slice(startItem, endItem).
+    css('display','table-row').animate({opacity:1}, 300);
+    if(prevPage >= 0) {
+        $('#nav a.active').removeClass('active')
+        $($('#nav a')[prevPage]).addClass('active');
+    }
+    if((Number(prevPage)) === 0) {
+        $('#prevPage').attr("disabled", true);
+    } else {
+        $('#prevPage').removeAttr("disabled");
+    }
+    $('#nextPage').removeAttr("disabled");
+}
+
 // Add pagination to the table
 const paginateTable = () => {
 
     $('#questionnaire-table').after('<div id="nav">Page: </div>');
-    var rowsShown = totalQuestions / paginationNumOfViews;
+    var rowsShown = totalQuestions / totalTablePages;
     var rowsTotal = $('#questionnaire-table tbody tr').length;
     var numPages = rowsTotal/rowsShown;
+    $('#nav').append('<button id="prevPage" type="button" onclick="paginatePrevQuestions()" disabled>Previous</button>')
     for(i = 0;i < numPages;i++) {
         var pageNum = i + 1;
         $('#nav').append('<a href="#/" rel="'+i+'">'+pageNum+'</a> ');
     }
+    $('#nav').append('<button id="nextPage" type="button" onclick="paginateNextQuestions()">Next</button>')
     $('#questionnaire-table tbody tr').hide();
     $('#questionnaire-table tbody tr').slice(0, rowsShown).show();
     $('#nav a:first').addClass('active');
-    $('#nav a').bind('click', function(){
+    $('#nav a').bind('click', function() {
         $('#nav a').removeClass('active');
         $(this).addClass('active');
         var currPage = $(this).attr('rel');
@@ -85,6 +132,18 @@ const paginateTable = () => {
         var endItem = startItem + rowsShown;
         $('#questionnaire-table tbody tr').css('opacity','0.0').hide().slice(startItem, endItem).
         css('display','table-row').animate({opacity:1}, 300);
+        
+        currPageNum = Number($(this)[0].innerHTML)
+        if((currPageNum - 1)  === 0) {
+            $('#prevPage').attr("disabled", true);
+        } else {
+            $('#prevPage').removeAttr("disabled");
+        }
+        if(currPageNum === totalTablePages) {
+            $('#nextPage').attr("disabled", true);
+        } else {
+            $('#nextPage').removeAttr("disabled");
+        }
     });
 }
 
@@ -110,34 +169,63 @@ function onChangeRadioButton(event) {
 }
 
 const getScoreCategory = (key, score) => {
-    if(key === 'activist') {
+    if(key === CATEGORY_ACTIVIST) {
         if(score > 12) return 'Very High'
         if(score > 10) return 'Strong'
         if(score > 6) return 'Moderate'
         if(score > 3) return 'Low'
         return 'Very Low'
     }
-    if(key === 'reflector') {
+    if(key === CATEGORY_REFLECTOR) {
         if(score > 17) return 'Very High'
         if(score > 14) return 'Strong'
         if(score > 11) return 'Moderate'
         if(score > 8) return 'Low'
         return 'Very Low'
     }
-    if(key === 'theorist') {
+    if(key === CATEGORY_THEORIST) {
         if(score > 15) return 'Very High'
         if(score > 13) return 'Strong'
         if(score > 10) return 'Moderate'
         if(score > 7) return 'Low'
         return 'Very Low'
     }
-    if(key === 'pragmatist') {
+    if(key === CATEGORY_PRAGMATIST) {
         if(score > 16) return 'Very High'
         if(score > 14) return 'Strong'
         if(score > 11) return 'Moderate'
         if(score > 7) return 'Low'
         return 'Very Low'
     }
+}
+
+const getCategoryCharacter = category => {
+    if(category === CATEGORY_ACTIVIST)
+        return `<img height="120" src="../assets/svg/active.svg" />`
+    if(category === CATEGORY_PRAGMATIST)
+        return `<img height="100" src="../assets/svg/prag.svg" />`
+    if(category === CATEGORY_REFLECTOR)
+        return `<img height="130" src="../assets/svg/reflect.svg" />`
+    if(category === CATEGORY_THEORIST)
+        return `<img height="120" src="../assets/svg/theory.svg" />`
+}
+
+const setRangeSliderBackground = category => {
+    if(category === CATEGORY_ACTIVIST)
+        return document.getElementById(`range_slider_${category}`).style = 
+            `background: linear-gradient(to right, white, #20315F)`
+
+    if(category === CATEGORY_PRAGMATIST)
+    return document.getElementById(`range_slider_${category}`).style = 
+        `background: linear-gradient(to right, white, #D11138)`
+
+    if(category === CATEGORY_REFLECTOR)
+    return document.getElementById(`range_slider_${category}`).style = 
+        `background: linear-gradient(to right, white, #939598)`
+
+    if(category === CATEGORY_THEORIST)
+    return document.getElementById(`range_slider_${category}`).style = 
+        `background: linear-gradient(to right, white, #000)`
 }
 
 // this function is called when Results button is clicked
@@ -163,6 +251,7 @@ const calResult = () => {
     
     
     // Display the score after user clicks submit and unhiding the section
+    $('#form-container').addClass('hidden')
     $('#result-div').removeClass('hidden')
     $('#show-results tbody').innerHTML = ``
 
@@ -171,7 +260,12 @@ const calResult = () => {
         let category = getScoreCategory(key, score)
         $('#show-results tbody').append(`
                 <tr>
-                    <td class="question_category_td">${key}</td>
+                    <td colspan="2" class="question_category_td">${key}</td>
+                </tr>
+                <tr>
+                    <td class="question_category_character">
+                        ${getCategoryCharacter(key)}
+                    </td>
                     <td class="score_td">
                         <div class="form-group slider">
                             <span class="slider_label">Very Low &nbsp;&nbsp;&nbsp;</span>
@@ -185,7 +279,7 @@ const calResult = () => {
                     </div>
                 </tr>
         `)
-        document.getElementById(`range_slider_${key}`).style = 'background: linear-gradient(to right, red, yellow, green)'
+        setRangeSliderBackground(key)
     }
 }
 
